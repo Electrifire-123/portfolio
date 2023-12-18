65db825e9c603f4a428b8fd594401539aae3d6b3
Rahul Yadav <99976384+Electrifire-123@users.noreply.github.com>
2023-12-18 10:26:48 +0530

    Update Details.ts

--- src/Details.ts
+++ src/Details.ts
@@ -190,33 +190,7 @@ export const projectDetails : ProjectDetails[] = [
     previewLink: "https://electrifire-123.github.io/digitalclock/",
     githubLink: "https://github.com/Electrifire-123/digitalclock",
   },
-  {
-    title: "Project title 4",
-    image: projectImage4,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: "HTML/CSS, JavaScript",
-    previewLink: "https://google.com",
-    githubLink: "https://github.com",
-  },
-  {
-    title: "Project title 5",
-    image: projectImage5,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: "HTML/CSS, JavaScript",
-    previewLink: "https://google.com",
-    githubLink: "https://github.com",
-  },
-  {
-    title: "Project title 6",
-    image: projectImage6,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: "HTML/CSS, JavaScript",
-    previewLink: "https://google.com",
-    githubLink: "https://github.com",
-  },
+  
 ];
 
 // Enter your Contact Details here
